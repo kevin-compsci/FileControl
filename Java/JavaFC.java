a09384c2024369b080424366bd01a96385b2980b
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-26 21:39:04 -0500

    Add files via upload

---
+++ Java/JavaFC.java
@@ -0,0 +1,26 @@
+/*
+Kevin Nguyen
+Basic Java program that will create, edit, open, and read files.
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaFC"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class JavaFC {
+	//Global Declarations
+
+	//main driver
+	public static void main(String[] args) {
+		//local Declarations
+		System.out.println("HelloWorld");
+	}
+
+	//function definitions below	
+}
