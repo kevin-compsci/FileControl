0a632a36417fd842ec3a4e0f2fadeb3cb875f579
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-26 22:08:15 -0500

    Add files via upload

--- Java/JavaFC.java
+++ Java/JavaFC.java
@@ -18,9 +18,76 @@ class JavaFC {
 
 	//main driver
 	public static void main(String[] args) {
-		//local Declarations
-		System.out.println("HelloWorld");
+		//create file
+		createMyFile();
+
+		//edit file
+		editMyFile();
+
+		//open file
+		openMyFile();
+
+		//read file
+		readMyFile();
+
+		//close file
+		closeMyFile();
 	}
 
 	//function definitions below	
+
+	//create a small text file
+	public static void createMyFile() {
+		try {
+			File myFile = new File("test.txt");
+
+			//if this file doesn't exist then create it
+			if(!file.exists()) {
+				myFile.createNewFile();
+			}
+		}
+		catch (IOException e) {
+			System.err.println("FILE CREATE ERROR");
+		}
+	}
+
+	//edit a file
+	public static void editMyFile() {
+		try {
+			System.out.println("WRITE")
+		}
+		catch (IOException e) {
+			System.err.println("FILE WRITE ERROR");
+		}
+	}
+
+	//open a file
+	public static void openMyFile() {
+		try {
+			System.out.println("OPEN")
+		}
+		catch (IOException e) {
+			System.err.println("FILE OPEN ERROR");
+		}
+	}
+
+	//read a file
+	public static void readMyFile() {
+		try {
+			System.out.println("READ")
+		}
+		catch (IOException e) {
+			System.err.println("FILE READ ERROR");
+		}
+	}
+
+	//close file
+	public static void closeMyFile() {
+		try {
+			System.out.println("CLOSE")
+		}
+		catch (IOException e) {
+			System.err.println("FILE CLOSE ERROR");
+		}
+	}
 }
